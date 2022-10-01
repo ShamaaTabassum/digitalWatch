77d6bee34e94dc9e4fc88d4bcd9b4c1829fe99b8
Shama Tabassum <shamatabassum071@gmail.com>
2022-10-01 14:36:50 +0500

    css updated

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,21 +1,7 @@
 import React, { useState, useEffect } from "react";
-// import GetDate from "../GetDate";
 
 function Time({ ...props }) {
-  // let [hour, setHour] = useState(0);
-  // let [minute, setMinute] = useState(0);
-  // let [second, setSecond] = useState(0);
-  let [ampm, setAmPm] = useState(true);
-
-  // useEffect(() => {
-  //   setInterval(() => {
-  //     let date = GetDate();
-  //     setHour(date.getHours());
-  //     setMinute(date.getMinutes());
-  //     setSecond(date.getSeconds());
-  //   }, 1000);
-  // }, []);
-
+  let [ampm, setAmPm] = useState(false);
   if (props.hour > 12) {
     props.setHour(props.hour - 12);
   }
